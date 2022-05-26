866bb1152a9c2cda7787004647a8eb134c7df6df
srhimanshu <himanshu.khandelwal@softraw.com>
2022-05-26 19:21:41 +0530

    headless browser

--- src/test/java/maven/Testa.java
+++ src/test/java/maven/Testa.java
@@ -2,6 +2,7 @@ package maven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,11 +12,13 @@ public class Testa
 	public void runChrome()
 	{
 		String chromeDriverPath = System.getProperty("user.dir")+"/chromedriver";
-		System.out.println(chromeDriverPath);
+//		System.out.println(chromeDriverPath);
 //		WebDriverManager.chromedriver().setup();
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("http://www.google.com");
-		// driver.quit();
+		driver.quit();
 	}
 }
