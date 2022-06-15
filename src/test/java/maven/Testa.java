5918bc1d2455208db869fcf0fb64cf917d6c6c74
himanshu.khandelwal@softraw.com <himanshu.khandelwal@softraw.com>
2022-06-15 20:04:49 +0530

    headless on

--- src/test/java/maven/Testa.java
+++ src/test/java/maven/Testa.java
@@ -35,7 +35,7 @@ public class Testa
 			String chromeDriverPath = System.getProperty("user.dir") + "/chromedriver";
 			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 			ChromeOptions options = new ChromeOptions();
-//			options.addArguments("--headless");
+			options.addArguments("--headless");
 			driver = new ChromeDriver(options);
 			driver.manage().window().maximize();
 			wait = new WebDriverWait(driver, Duration.ofSeconds(20));
@@ -60,7 +60,7 @@ public class Testa
 			String firefoxDriverPath = System.getProperty("user_dir")+"/geckodriver";
 			System.setProperty("webdriver.firefox.driver", firefoxDriverPath);
 			FirefoxOptions options = new FirefoxOptions();
-//			options.addArguments("--headless");
+			options.addArguments("--headless");
 			driver = new FirefoxDriver(options);
 			driver.manage().window().maximize();
 			wait = new WebDriverWait(driver, Duration.ofSeconds(20));
